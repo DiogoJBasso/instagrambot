2f28802765ee961c804d88f15dbaf81d1c0eee8f
Diogo Basso <65865529+DiogoJBasso@users.noreply.github.com>
2020-09-30 10:07:33 -0300

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const ig = require('./instagram');
 (async()=>{
     await ig.initialize();
 
-    await ig.login('test.account.bot', 'lixo.tralha1')//CONFIGURAÇÕES: COLOCAR O NOME DE USUÁRIO E SENHA RESPECTIVAMENTE
+    await ig.login('user', 'senha')//CONFIGURAÇÕES: COLOCAR O NOME DE USUÁRIO E SENHA RESPECTIVAMENTE
 
     await ig.engajamento();
 })()
